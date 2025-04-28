1214df446d789b6c03d9cfe2b6bc06064404eff9
cracking-exams <shardul686@gmail.com>
2025-04-29 00:26:16 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,23 @@
+import nltk
+from nltk.corpus import wordnet
+
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+def describe(word):
+    synets = wordnet.synsets(word)
+    synonyms = {lemma.name() for syn in synets for lemma in syn.lemmas()}
+
+    antonyms = {ant.name() for syn in synets for lemma in syn.lemmas() for ant in lemma.antonyms()}
+
+    hyponyms = {hyp.name().split('.')[0] for syn in synets for hyp in syn.hyponyms()}
+
+    hypernyms = {hyper.name().split('.')[0] for syn in synets for hyper in syn.hypernyms()}
+
+    print(f"Word: {word}")
+    print(f"Synonyms: {synonyms}")
+    print(f"Antonyms: {antonyms}")
+    print(f"Hypernyms: {hypernyms}")
+    print(f"Hyponyms: {hyponyms}")
+
+describe('car')
